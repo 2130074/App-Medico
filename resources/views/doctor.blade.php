b5169e2c6039d3ff01042e5e46e2176e48d4d4b6
2130074 <2130074@upv.edu.mx>
2024-06-17 21:36:00 -0600

    Vista de pago y ver pacientes lista

--- resources/views/doctor.blade.php
+++ resources/views/doctor.blade.php
@@ -27,15 +27,20 @@
                     </li>
                 </ul>
             </div>
-            <a href="{{ route('logout') }}"><button
+           <button
                     class="w-full flex justify-center py-2 px-2 border border-transparent text-sm font-medium rounded-md text-white bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                     onclick="location.href='/'">
                     Cerrar sesión
             </button>
         </div>
 
-
     </div>
 </body>
 
 </html>
+
+
+
+
+
+
--- resources/views/pago.blade.php
+++ resources/views/pago.blade.php
@@ -5,71 +5,109 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <title>Ver Pago</title>
+    <title>Ver Pacientes</title>
 </head>
 
-<body
-    style="background-image: url('img/fondo.jpg'); background-size: cover; background-position: center; height: 100vh; margin: 0; padding: 0;">
-    <header class="flex justify-between items-center bg-opacity-75 bg-white p-4">
-        <h1 class="text-2xl font-bold text-gray-800">SaludConecta</h1>
-        <a href="{{ route('logout') }}"><button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Cerrar sesión</button></a>
-    </header>
-
-    <div class="flex justify-center mt-6">
-        <div class="relative w-2/3 max-w-2xl">
-            <input type="text" placeholder="Buscar"
-                class="w-full py-2 pl-4 pr-10 border border-blue-500 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500">
-            <svg class="absolute right-3 top-2.5 w-5 h-5 text-blue-500" xmlns="http://www.w3.org/2000/svg"
-                fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M21 21l-4.35-4.35M16.44 11.2a5.45 5.45 0 11-10.89 0 5.45 5.45 0 0110.89 0z" />
-            </svg>
+<body class="bg-gradient-to-r from-[#4CA9DF] to-[#292E91]">
+    <div class="flex h-screen">
+        <div class="bg-blue-300 text-white w-1/5 p-6 flex flex-col justify-between shadow-xl">
+            <div>
+                <div class="flex items-center mb-8">
+                    <img src="img/logo.png" alt="Logo" class="w-8 h-8 mr-2">
+                    <span class="text-2xl font-bold">Salud Conecta</span>
+                </div>
+                <ul>
+                    <li class="flex items-center mb-10">
+                        <img src="img/calendario.png" alt="Registrar Icon" class="w-6 h-6 mr-2">
+                        <a href="/recepcionista" class="text-lg">Agenda</a>
+                    </li>
+                    <li class="flex items-center mb-10">
+                        <img src="img/calendario.png" alt="Registrar Icon" class="w-6 h-6 mr-2">
+                        <a href="/servicios" class="text-lg">Agregar servicio</a>
+                    </li>
+                    <li class="flex items-center mb-10">
+                        <img src="img/calendario.png" alt="Registrar Icon" class="w-6 h-6 mr-2">
+                        <a href="/registroPacientes" class="text-lg">Agregar paciente</a>
+                    </li>
+                    <li class="flex items-center mb-10">
+                        <img src="img/usuario.png" alt="Ver Usuarios Icon" class="w-6 h-6 mr-2">
+                        <a href="/verPacientes" class="text-lg">Ver pacientes</a>
+                    </li>
+                </ul>
+            </div>
+            <button
+                class="w-full flex justify-center py-2 px-2 border border-transparent text-sm font-medium rounded-md text-white bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                onclick="location.href='/'">
+                Cerrar sesión
+            </button>
         </div>
-    </div>
 
-    <div class="flex justify-center mt-6">
-        <div class="bg-white bg-opacity-75 p-8 rounded-lg shadow-md w-full max-w-3xl">
-            <h2 class="text-3xl font-bold text-blue-600 text-center mb-4">Detalles del pago</h2>
-
-            <div class="flex-grow flex items-center justify-center mt-6">
-                <div class="w-full max-w-xl bg-white bg-opacity-75 rounded-lg p-6">
-                    <table class="min-w-full bg-white border border-gray-300">
-                        <thead>
-                            <tr>
-                                <th class="py-2 px-4 border-b border-gray-300">Servicio</th>
-                                <th class="py-2 px-4 border-b border-gray-300">Costo</th>
-                                <th class="py-2 px-4 border-b border-gray-300">Fecha</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td class="py-2 px-4 border-b border-gray-300">Consulta General</td>
-                                <td class="py-2 px-4 border-b border-gray-300 text-center">$50.00</td>
-                                <td class="py-2 px-4 border-b border-gray-300 text-center">12/08/2023</td>
-                            </tr>
-                            <tr>
-                                <td class="py-2 px-4 border-b border-gray-300">Examen de Sangre</td>
-                                <td class="py-2 px-4 border-b border-gray-300 text-center">$30.00</td>
-                                <td class="py-2 px-4 border-b border-gray-300 text-center">20/05/2024</td>
-                            </tr>
-                            <tr>
-                                <td class="py-2 px-4 border-b border-gray-300">Radiografía</td>
-                                <td class="py-2 px-4 border-b border-gray-300 text-center">$40.00</td>
-                                <td class="py-2 px-4 border-b border-gray-300 text-center">25/07/2024</td>
-                            </tr>
-                        </tbody>
-                    </table>
+        <div class="flex-1 flex flex-col p-6">
+            <div class="flex justify-center mt-6">
+                <div class="relative w-2/3 max-w-2xl">
+                    <input type="text" placeholder="Buscar"
+                        class="w-full py-2 pl-4 pr-10 border border-blue-500 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <svg class="absolute right-3 top-2.5 w-5 h-5 text-blue-500" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M21 21l-4.35-4.35M16.44 11.2a5.45 5.45 0 11-10.89 0 5.45 5.45 0 0110.89 0z" />
+                    </svg>
                 </div>
             </div>
 
-            <div class="flex justify-end">
+            <div class="overflow-x-auto mt-6">
+                <table class="min-w-full bg-white bg-opacity-10 rounded-lg shadow-xl text-white">
+                    <thead>
+                        <tr class="bg-blue-500">
+                            <th class="py-2 px-4 text-left">Servicio</th>
+                            <th class="py-2 px-4 text-left">Costo</th>
+                            <th class="py-2 px-4 text-left">Fecha</th>
+                            <th class="py-2 px-4 text-left">Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="hover:bg-blue-600">
+                            <td class="py-2 px-4">Consulta General</td>
+                            <td class="py-2 px-4 text-center">$50.00</td>
+                            <td class="py-2 px-4 text-center">20/05/2024</td>
+                            <td class="py-2 px-4 text-center">
+                                <button
+                                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onclick="location.href='/pago'">
+                                    Eliminar</button>
+                            </td>
+                        </tr>
+                        <tr class="hover:bg-blue-600">
+                            <td class="py-2 px-4">Consulta General</td>
+                            <td class="py-2 px-4 text-center">$50.00</td>
+                            <td class="py-2 px-4 text-center">20/05/2024</td>
+                            <td class="py-2 px-4 text-center">
+                                <button
+                                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onclick="location.href='/pago'">
+                                    Eliminar</button>
+                            </td>
+                        </tr>
+                        <tr class="hover:bg-blue-600">
+                            <td class="py-2 px-4">Consulta General</td>
+                            <td class="py-2 px-4 text-center">$50.00</td>
+                            <td class="py-2 px-4 text-center">20/05/2024</td>
+                            <td class="py-2 px-4 text-center">
+                                <button
+                                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onclick="location.href='/pago'">
+                                    Eliminar</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="flex justify-end mt-6">
                 <h3 class="text-xl font-bold text-gray-800">Total: <span class="text-blue-500">$120.00</span></h3>
             </div>
 
             <div class="flex justify-end mt-6">
                 <button type="button"
                     class=" flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                    onclick="location.href='/recepcionista'">
+                    onclick="location.href='/verPacientes'">
                     Regresar
                 </button>
             </div>
