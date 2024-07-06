d05ef2ed721c2a6251eac9c3571f6bcd282e8a03
2130074 <2130074@upv.edu.mx>
2024-07-06 13:14:09 -0600

    Cambio total de la Agenda

--- resources/views/recepcionista.blade.php
+++ resources/views/recepcionista.blade.php
@@ -6,167 +6,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     <script src="https://cdn.tailwindcss.com"></script>
+    <!-- FullCalendar CSS and JS -->
+    <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.css' rel='stylesheet' />
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.js'></script>
     <title>Bienvenida recepcionista</title>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const date = new Date();
-            let currentMonth = date.getMonth();
-            let currentYear = date.getFullYear();
+    <style>
+        /* Ajustar el estilo del calendario */
+        #calendar {
+            background-color: white;
+            max-width: 100%;
+            margin: 0 auto;
+        }
 
-            const months = [
-                'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
-                'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
-            ];
+        /* Estilo para el modal */
+        #modal {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            justify-content: center;
+            align-items: center;
+            z-index: 1000;
+        }
 
-
-            const occupiedHours = {
-                '2024-06-24': ['08:00', '09:00'],
-                '2024-06-25': ['10:00', '11:00'],
-            };
-
-            function renderCalendar(month, year) {
-                const firstDay = new Date(year, month).getDay();
-                const daysInMonth = new Date(year, month + 1, 0).getDate();
-
-                const calendarBody = document.getElementById('calendar-body');
-                calendarBody.innerHTML = '';
-
-                let date = 1;
-                for (let i = 0; i < 6; i++) {
-                    const row = document.createElement('tr');
-
-                    for (let j = 0; j < 7; j++) {
-                        const cell = document.createElement('td');
-                        cell.classList.add('border', 'p-2', 'text-center', 'cursor-pointer');
-
-                        if (i === 0 && j < firstDay) {
-                            const cellText = document.createTextNode('');
-                            cell.appendChild(cellText);
-                        } else if (date > daysInMonth) {
-                            break;
-                        } else {
-                            const cellText = document.createTextNode(date);
-                            cell.appendChild(cellText);
-                            const fullDate =
-                                `${year}-${String(month + 1).padStart(2, '0')}-${String(date).padStart(2, '0')}`;
-                            cell.setAttribute('data-date', fullDate);
-
-                            if (new Date(fullDate) < new Date().setHours(0, 0, 0, 0)) {
-                                cell.classList.add('text-gray-400', 'cursor-not-allowed');
-                            } else {
-                                cell.addEventListener('click', openModal);
-                            }
-                            date++;
-                        }
-
-                        row.appendChild(cell);
-                    }
-
-                    calendarBody.appendChild(row);
-                }
-
-                document.getElementById('monthAndYear').innerText = `${months[month]} ${year}`;
-            }
-
-            function openModal(event) {
-                const modal = document.getElementById('modal');
-                modal.classList.remove('hidden');
-                modal.classList.add('flex');
-                const selectedDate = event.target.getAttribute('data-date');
-                document.getElementById('selected-date').value = selectedDate;
-
-                const timeSelect = document.getElementById('selected-time');
-                const availableHours = [
-                    '08:00', '09:00', '10:00', '11:00', '12:00',
-                    '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'
-                ];
-                timeSelect.innerHTML = '';
-                availableHours.forEach(time => {
-                    const option = document.createElement('option');
-                    option.value = time;
-                    option.text = time;
-
-                    if (occupiedHours[selectedDate] && occupiedHours[selectedDate].includes(time)) {
-                        option.disabled = true;
-                    }
-
-                    timeSelect.appendChild(option);
-                });
-            }
-
-            function closeModal() {
-                const modal = document.getElementById('modal');
-                modal.classList.remove('flex');
-                modal.classList.add('hidden');
-            }
-
-            function validateForm(event) {
-                const patientName = document.getElementById('patient-name').value;
-                const appointmentReason = document.getElementById('appointment-reason').value;
-                const serviceType = document.getElementById('service-type').value;
-                const selectedDate = document.getElementById('selected-date').value;
-                const selectedTime = document.getElementById('selected-time').value;
-
-                if (!patientName || !appointmentReason || !serviceType || !selectedDate || !selectedTime) {
-                    event.preventDefault();
-                    alert('Por favor, completa todos los campos.');
-                    return;
-                }
-
-                if (occupiedHours[selectedDate] && occupiedHours[selectedDate].includes(selectedTime)) {
-                    event.preventDefault();
-                    alert('La hora seleccionada ya está ocupada.');
-                }
-            }
-
-            document.getElementById('next').addEventListener('click', function() {
-                currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-                currentMonth = (currentMonth + 1) % 12;
-                renderCalendar(currentMonth, currentYear);
-            });
-
-            document.getElementById('prev').addEventListener('click', function() {
-                currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-                currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-                renderCalendar(currentMonth, currentYear);
-            });
-
-            document.getElementById('today').addEventListener('click', function() {
-                currentMonth = date.getMonth();
-                currentYear = date.getFullYear();
-                renderCalendar(currentMonth, currentYear);
-            });
-
-            document.getElementById('close-modal').addEventListener('click', closeModal);
-            document.getElementById('register-button').addEventListener('click', validateForm);
-
-            renderCalendar(currentMonth, currentYear);
-        });
-
-        $(document).ready(function() {
-            // Llenar el select de pacientes
-            $.getJSON("/api/pacientes", function(data) {
-                var select = $("#patient-name");
-                select.empty(); // Vacía el select antes de llenarlo
-                $.each(data, function(key, val) {
-                    select.append("<option value='" + key + "'>" + val + "</option>");
-                });
-            });
-
-            // Llenar el select de servicios
-            $.getJSON("/api/servicios", function(data) {
-                var select = $("#service-type");
-                select.empty(); // Vacía el select antes de llenarlo
-                $.each(data, function(key, val) {
-                    select.append("<option value='" + key + "'>" + val + "</option>");
-                });
-            });
-        });
-    </script>
+        #modal .modal-content {
+            background-color: white;
+            border-radius: 0.5rem;
+            padding: 1rem;
+            width: 90%;
+            max-width: 500px;
+            max-height: 90%;
+            overflow-y: auto;
+        }
+    </style>
 </head>
 
 <body class="bg-gradient-to-r from-[#4CA9DF] to-[#292E91]">
-    <div class="flex h-screen">
+    <div class="flex h-screen overflow-hidden">
         <div class="bg-blue-650 text-white w-1/5 p-6 flex flex-col justify-between shadow-xl">
             <div>
                 <div class="flex items-center mb-8">
@@ -196,62 +75,22 @@
                     </li>
                 </ul>
             </div>
-            <div class="my-4">
-                @session('succes')
-                    <div class="alert alert-success" role="alert">
-                        {{ $value }}
-                    </div>
-                @endsession
-            </div>
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            <button
-                class="w-full flex justify-center py-2 px-2 border border-transparent text-sm font-medium rounded-md text-white bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                onclick="location.href='/'">
+            <button class="w-full flex justify-center py-2 px-2 border border-transparent text-sm font-medium rounded-md text-white bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" onclick="location.href='/'">
                 Cerrar sesión
             </button>
         </div>
-
-        <div class="flex-1 p-8 mt-24">
-            <div class="bg-white shadow-md rounded-lg p-4">
-                <div class="flex justify-between mb-4">
-                    <button id="prev" class="text-blue-600 hover:underline mr-2">&lt;</button>
-                    <h2 id="monthAndYear" class="text-xl font-bold">Enero 2022</h2>
-                    <button id="next" class="text-blue-600 hover:underline mr-2">&gt;</button>
-                    <button id="today" class="bg-blue-600 text-white py-1 px-3 rounded">Hoy</button>
-                </div>
-                <table class="table-auto w-full">
-                    <thead>
-                        <tr>
-                            <th class="p-2">Lun</th>
-                            <th class="p-2">Mar</th>
-                            <th class="p-2">Mié</th>
-                            <th class="p-2">Jue</th>
-                            <th class="p-2">Vie</th>
-                            <th class="p-2">Sáb</th>
-                            <th class="p-2">Dom</th>
-                        </tr>
-                    </thead>
-                    <tbody id="calendar-body" class="text-center">
-                        <!-- Los dias se generan aqui -->
-                    </tbody>
-                </table>
-            </div>
+        <!-- Calendario -->
+        <div class="w-4/5 p-6 overflow-auto">
+            <div id='calendar'></div>
         </div>
     </div>
 
-    <div id="modal" class="hidden fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
-        <div class="bg-white rounded-lg p-6 w-1/3 relative">
+    <!-- Modal -->
+    <div id="modal" class="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
+        <div class="modal-content bg-white rounded-lg p-6 w-1/3 relative">
             <button id="close-modal" class="absolute top-2 right-2 text-gray-700 hover:text-gray-900">&times;</button>
             <h2 class="text-2xl font-bold mb-4">Agendar servicio</h2>
-        
+
             @if (isset($pacientes) && isset($servicios))
                 <form id="appointment-form" method="POST" action="{{ route('recepcionista.store') }}">
                     @csrf
@@ -316,6 +155,102 @@
             @endif
         </div>
     </div>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialDate: '2023-01-12',
+                initialView: 'timeGridWeek',
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                },
+                height: 'auto',
+                navLinks: true, // can click day/week names to navigate views
+                editable: true,
+                selectable: true,
+                selectMirror: true,
+                nowIndicator: true,
+                events: [
+                    {
+                        title: 'All Day Event',
+                        start: '2023-01-01',
+                    },
+                    {
+                        title: 'Long Event',
+                        start: '2023-01-07',
+                        end: '2023-01-10'
+                    },
+                    {
+                        groupId: 999,
+                        title: 'Repeating Event',
+                        start: '2023-01-09T16:00:00'
+                    },
+                    {
+                        groupId: 999,
+                        title: 'Repeating Event',
+                        start: '2023-01-16T16:00:00'
+                    },
+                    {
+                        title: 'Conference',
+                        start: '2023-01-11',
+                        end: '2023-01-13'
+                    },
+                    {
+                        title: 'Meeting',
+                        start: '2023-01-12T10:30:00',
+                        end: '2023-01-12T12:30:00'
+                    },
+                    {
+                        title: 'Lunch',
+                        start: '2023-01-12T12:00:00'
+                    },
+                    {
+                        title: 'Meeting',
+                        start: '2023-01-12T14:30:00'
+                    },
+                    {
+                        title: 'Happy Hour',
+                        start: '2023-01-12T17:30:00'
+                    },
+                    {
+                        title: 'Dinner',
+                        start: '2023-01-12T20:00:00'
+                    },
+                    {
+                        title: 'Birthday Party',
+                        start: '2023-01-13T07:00:00'
+                    },
+                    {
+                        title: 'Click for Google',
+                        url: 'http://google.com/',
+                        start: '2023-01-28'
+                    }
+                ],
+                dateClick: function(info) {
+                    // Abrir el modal
+                    var modal = document.getElementById('modal');
+                    modal.style.display = 'flex';
+
+                    // Poner la fecha seleccionada en el campo de fecha del formulario
+                    var selectedDate = document.getElementById('selected-date');
+                    selectedDate.value = info.dateStr;
+                }
+            });
+
+            calendar.render();
+
+            // Cerrar el modal
+            var closeModalBtn = document.getElementById('close-modal');
+            closeModalBtn.addEventListener('click', function() {
+                var modal = document.getElementById('modal');
+                modal.style.display = 'none';
+            });
+        });
+    </script>
 </body>
 
 </html>
